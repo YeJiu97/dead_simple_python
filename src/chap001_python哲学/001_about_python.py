34312eebd26f89d085731a2f53752d2a73b0f725
yejiu97 <wangjun.shen.1997@outlook.com>
2026-01-04 20:46:59 +0800

    chap0102

---
+++ src/chap001_python哲学/001_about_python.py
@@ -0,0 +1,41 @@
+# python是一门图灵完备的语言
+
+# python的解释器使用了CPython来进行，速度比原生机械码来的慢，但是有各种库和其他的解释器实现，比如说pypy
+
+# CPython的性能瓶颈只会在一些特定的场合才会是一个问题
+
+# Python是解释型语言，Python代码也可以被翻译为机器码，但是这种情况比较少见
+
+# Python 2.7 是 Python2 的最后一个版本代码，之后都是Python3, 许多老项目已经无法使用了，对于不需要再被支持老Python的项目，通常使用Python3来进行实现
+
+# Python3是动态类型的，但是可以使用Python的类型系统来辅助，而不是与之抗争
+
+# Python有一个标准库，可以使用2to3来将Python2代码自动转换为Python3代码
+
+# Python之禅可以使用 import this 来进行打印
+
+# The Zen of Python, by Tim Peters
+
+# Beautiful is better than ugly.
+# Explicit is better than implicit.
+# Simple is better than complex.
+# Complex is better than complicated.
+# Flat is better than nested.
+# Sparse is better than dense.
+# Readability counts.
+# Special cases aren't special enough to break the rules.
+# Although practicality beats purity.
+# Errors should never pass silently.
+# Unless explicitly silenced.
+# In the face of ambiguity, refuse the temptation to guess.
+# There should be one-- and preferably only one --obvious way to do it.
+# Although that way may not be obvious at first unless you're Dutch.
+# Now is better than never.
+# Although never is often better than *right* now.
+# If the implementation is hard to explain, it's a bad idea.
+# If the implementation is easy to explain, it may be a good idea.
+# Namespaces are one honking great idea -- let's do more of those!
+# [Finished in 502ms]
+
+
+# Python是开源的，并且社区也是开放的
---
+++ src/chap002_python开发环境/003_running_python_file.py
@@ -0,0 +1,3 @@
+# python3 003_running_python_file.py
+
+print("hello world")
